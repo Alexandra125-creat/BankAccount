70da51d365274a8746ebef53c2c490f8ba92ed08
typesew <brichkovzahar@gmail.com>
2026-03-06 01:52:14 +0300

    BankAccount class is ready

--- include/BankAccount.h
+++ include/BankAccount.h
@@ -1,6 +1,24 @@
-#include <iostream>
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
 
-int main() {
+#include <string>
 
-    return 0;
-}
+class BankAccount{
+    private:
+    std::string accountNumber;
+    std::string ownerName;
+    double balance;
+
+    public:
+    BankAccount(const std::string& accNum, const std::string& owner, double initialBalance = 0.0);
+    void deposit(double amount);
+    bool withdraw(double amount);
+    double getBalance() const;
+    std::string getAccountNumber() const;
+    std::string getOwnerName() const;
+    void printStatement() const;
+
+};
+
+
+#endif
--- src/BankAccount.cpp
+++ src/BankAccount.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <BankAccount.h>
 
-int main(){
+BankAccount::BankAccount( const std::string& accNum, const std::string& owner,
+     double initialBalance)
+     : accountNumber(accNum), ownerName(owner), balance(initialBalance) {}
 
-    return 0;
+void BankAccount::deposit(double amount){
+    if(amount > 0) balance += amount;
+}
+
+bool BankAccount::withdraw(double amount){
+    if( balance > 0 && balance >= amount){
+        balance -= amount;
+        return true;
+    }
+    return false;
+}
+
+double BankAccount::getBalance() const {
+    return balance;
+}
+
+std::string BankAccount::getAccountNumber() const {
+    return accountNumber;
+}
+
+std::string BankAccount::getOwnerName() const {
+    return ownerName;
+}
+
+void BankAccount::printStatement() const {
+    std::cout << "Номер счета: " << accountNumber << std::endl << "Имя держателя счета: " << ownerName << std::endl << "Баланс: " << balance << std::endl;
 }
